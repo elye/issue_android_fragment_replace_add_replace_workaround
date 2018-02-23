57af880c01241f2c717430e75bd30a2e3b7b4f23
elye <elye.project@gmail.com>
2018-02-24 09:40:05 +1100

    Initial commit

---
+++ app/src/main/java/com/elyeproj/myapplication/MainActivity.kt
@@ -0,0 +1,70 @@
+package com.elyeproj.myapplication
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        add_f1.setOnClickListener {
+            addFragment(OneFragment(),  "OneFragment")
+        }
+
+        replace_f1.setOnClickListener {
+            replaceFragment(OneFragment(),  "OneFragment")
+        }
+
+        add_f2.setOnClickListener {
+            addFragment(TwoFragment(),  "TwoFragment")
+        }
+
+        replace_f2.setOnClickListener {
+            replaceFragment(TwoFragment(),  "TwoFragment")
+        }
+
+        add_f3.setOnClickListener {
+            addFragment(ThreeFragment(),  "ThreeFragment")
+        }
+
+        replace_f3.setOnClickListener {
+            replaceFragment(ThreeFragment(),  "ThreeFragment")
+        }
+
+        add_f4.setOnClickListener {
+            addFragment(FourFragment(),  "FourFragment")
+        }
+
+        replace_f4.setOnClickListener {
+            replaceFragment(FourFragment(),  "FourFragment")
+        }
+
+        supportFragmentManager.addOnBackStackChangedListener {
+            val currentFragment = supportFragmentManager.findFragmentById(R.id.container)
+            currentFragment?.onResume()
+        }
+    }
+
+    private fun addFragment(fragment: Fragment, name: String) {
+        supportFragmentManager.beginTransaction().add(R.id.container, fragment, name)
+                .addToBackStack(name).commit()
+    }
+
+    private fun replaceFragment(fragment: Fragment, name: String) {
+        supportFragmentManager.beginTransaction().replace(R.id.container, fragment, name)
+                .addToBackStack(name).commit()
+    }
+
+    override fun onBackPressed() {
+        if (supportFragmentManager.backStackEntryCount > 0) {
+            supportFragmentManager.popBackStackImmediate()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
