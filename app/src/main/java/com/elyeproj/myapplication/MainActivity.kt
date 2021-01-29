ec8407a7f6dd99eb00ee7f46f3eb5d56e79f9b64
Elisha Lye <elisha.lye@rea-group.com>
2021-01-30 00:57:02 +1100

    Upgrade to Android X

--- app/src/main/java/com/elyeproj/myapplication/MainActivity.kt
+++ app/src/main/java/com/elyeproj/myapplication/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.elyeproj.myapplication
 
 import android.os.Bundle
-import android.support.v4.app.Fragment
-import android.support.v4.app.FragmentManager
-import android.support.v7.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
